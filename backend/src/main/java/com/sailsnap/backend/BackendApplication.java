9f9faae801e0def4ac6f70accef0d78992b88100
pmelvin43 <prestonmelvin4373@gmail.com>
2025-07-07 21:32:08 -0700

    added spring boot template

---
+++ backend/src/main/java/com/sailsnap/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.sailsnap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) // tell it to excluse the DB config as it's not
+																		// currently setup yet
+@RestController
+public class BackendApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ backend/src/test/java/com/sailsnap/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sailsnap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
