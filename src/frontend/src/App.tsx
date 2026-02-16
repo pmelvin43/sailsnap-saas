5a8a0989b0ec21e449ff900d8567d240093ab678
Imoudu Ibrahim <72046339+imoudu13@users.noreply.github.com>
2026-02-15 19:47:18 -0800

    Added Cognito auth provider

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAuth } from "react-oidc-context";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const auth = useAuth();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const signOutRedirect = () => {
+        const clientId = "7ou4s0s3nbaprr9q8eh9kh06h2";
+        const logoutUri = "<logout uri>";
+        const cognitoDomain = "https://<user pool domain>";
+        window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+    };
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Encountering error... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+            <div>
+                <pre> Hello: {auth.user?.profile.email} </pre>
+                <pre> ID Token: {auth.user?.id_token} </pre>
+                <pre> Access Token: {auth.user?.access_token} </pre>
+                <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+
+                <button onClick={() => auth.removeUser()}>Sign out</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <button onClick={() => auth.signinRedirect()}>Sign in</button>
+            <button onClick={() => signOutRedirect()}>Sign out</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot }from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const cognitoAuthConfig = {
+    authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_NyEcKzFV5",
+    client_id: "7ou4s0s3nbaprr9q8eh9kh06h2",
+    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+    response_type: "code",
+    scope: "phone openid email",
+};
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
+    <StrictMode>
+        <AuthProvider {...cognitoAuthConfig}>
+            <App />
+        </AuthProvider>
+    </StrictMode>
+);
